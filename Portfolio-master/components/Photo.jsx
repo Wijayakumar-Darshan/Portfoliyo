eede8241816dbd6ea65e277356c8a8cd2d66f984
Wijayakumar Darshan <cb8b14611@kiu.ac.lk>
2025-04-18 15:59:13 +0530

    Update Photo.jsx

--- Portfolio-master/components/Photo.jsx
+++ Portfolio-master/components/Photo.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="h-full w-full relative z-10 group">
+    <div className="h-full w-full relative z-10 group flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{
@@ -16,11 +16,11 @@ const Photo = () => {
             ease: "easeIn",
           },
         }}
-        className="relative bg-primary rounded-full"
+        className="relative rounded-full overflow-hidden"
       >
-        {/* Circle */}
+        {/* Animated Circles */}
         <motion.svg
-          className="w-[300px] h-[300px] xl:w-[380px] xl:h-[380px] absolute top-0 left-0"
+          className="w-[300px] h-[300px] xl:w-[380px] xl:h-[380px]"
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
@@ -33,9 +33,7 @@ const Photo = () => {
             strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{
-              strokeDasharray: "24 10 0 0",
-            }}
+            initial={{ strokeDasharray: "24 10 0 0" }}
             whileInView={{
               strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
               rotate: [120, 360],
@@ -55,9 +53,7 @@ const Photo = () => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{
-              strokeDasharray: "24 10 0 0",
-            }}
+            initial={{ strokeDasharray: "24 10 0 0" }}
             whileInView={{
               strokeDasharray: ["2 255 15 5", "12 22 55 42"],
               rotate: [90, 360],
@@ -77,9 +73,7 @@ const Photo = () => {
             strokeWidth="1"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{
-              strokeDasharray: "24 10 0 0",
-            }}
+            initial={{ strokeDasharray: "24 10 0 0" }}
             whileInView={{
               strokeDasharray: ["2 255 15 5", "2 155 95 15"],
               rotate: [90, 360],
@@ -92,7 +86,7 @@ const Photo = () => {
           />
         </motion.svg>
 
-        {/* Image */}
+        {/* Image Container */}
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{
@@ -103,23 +97,26 @@ const Photo = () => {
               ease: "easeInOut",
             },
           }}
-          whileHover={{ scale: 1.1, rotate: 2 }}
+          whileHover={{ scale: 1.05 }}
           transition={{
-            duration: 0.5,
+            duration: 0.3,
             type: "spring",
             stiffness: 300,
-            damping: 8,
+            damping: 10,
           }}
-          className="relative w-[298px] h-[298px] xl:w-[380px] xl:h-[380px] rounded-full overflow-hidden mix-blend-lighten"
+          className="absolute inset-0 flex items-center justify-center"
         >
-          <Image
-            src="/assets/dp/my photo.jpg"
-            priority
-            quality={100}
-            fill
-            alt=""
-            className="object-contain p-6 rounded-full brightness-150"
-          />
+          <div className="relative w-[260px] h-[260px] xl:w-[330px] xl:h-[330px] rounded-full overflow-hidden border-4 border-transparent group-hover:border-[#01ffbb] transition-all duration-300">
+            <Image
+              src="/assets/dp/my photo.jpg"
+              priority
+              quality={100}
+              fill
+              alt="Profile Photo"
+              className="object-cover"
+              sizes="(max-width: 768px) 260px, 330px"
+            />
+          </div>
         </motion.div>
       </motion.div>
     </div>
